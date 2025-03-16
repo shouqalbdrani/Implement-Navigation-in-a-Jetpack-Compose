7fbf8ad8080a4c940c1fcbbc981f2457125d7d4f
shouq <shouqalbdrani@gmail.com>
2025-03-16 16:55:47 +0300

    Add Bottom Bar

--- app/src/main/java/com/example/navigationpro/MainActivity.kt
+++ app/src/main/java/com/example/navigationpro/MainActivity.kt
@@ -9,6 +9,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -18,6 +24,13 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.*
 
 import com.example.navigationpro.ui.theme.NavigationProTheme
+
+data class BottomNavigationItem( // add property of item in navigation bar title of icon , selected or not and the route if the user Select
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val route: String
+)
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -38,6 +51,9 @@ class MainActivity : ComponentActivity() {
                             showBackButton = showBackButton,
                             onBackClick = { navController.popBackStack() }
                         )
+                    },
+                    bottomBar = {
+                        BottomNavigationBar(navController)
                     }
                 ) { innerPadding ->
                     NavigationHost(navController, innerPadding) { newTitle, showBack ->
@@ -46,6 +62,46 @@ class MainActivity : ComponentActivity() {
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController) {
+    val items = listOf(
+        BottomNavigationItem("Home",
+            Icons.Filled.Home,
+            Icons.Outlined.Home,
+            "FirstScreen"),
+        BottomNavigationItem("Profile",
+            Icons.Filled.Person,
+            Icons.Outlined.Person,
+            "SecondScreen"),
+        BottomNavigationItem("Settings",
+            Icons.Filled.Settings,
+            Icons.Outlined.Settings,
+            "ThirdScreen")
+    )
+
+    NavigationBar {
+        val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route // take the current route
+
+        items.forEach { item ->
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        if (currentRoute == item.route) item.selectedIcon else item.unselectedIcon, // change the state if the icon selected
+                        contentDescription = item.title
+                    )
+                },
+                label = { Text(item.title) }, // the title of icon
+                selected = currentRoute == item.route, // check if the icon selected
+                onClick = {
+                    navController.navigate(item.route) {
+                        popUpTo("FirstScreen") { inclusive = false } // does happen any replicate and for Ensure the first screen in the stack
+                    }
+                }
+            )
         }
     }
 }
@@ -62,8 +118,7 @@ fun CustomTopAppBar(
         navigationIcon = {
             if (showBackButton) {
                 IconButton(onClick = onBackClick) {
-                    Icon(imageVector = Icons.Default.ArrowBack,
-                        contentDescription = "Back")
+                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back")
                 }
             }
         }
@@ -86,11 +141,11 @@ fun NavigationHost(
             FirstScreen(navController)
         }
         composable("SecondScreen") {
-            setTitle("Second Screen", true) // show the button o back if true
+            setTitle("Second Screen", true)
             SecondScreen(navController)
         }
         composable("ThirdScreen") {
-            setTitle("Third Screen", true) // show the button o back if true
+            setTitle("Third Screen", true)
             ThirdScreen(navController)
         }
     }
