d73bb035cfe9861390b7aa9143de0b88ca90b52e
shouq <shouqalbdrani@gmail.com>
2025-03-16 15:26:28 +0300

    Make the top bar change dynamically

--- app/src/main/java/com/example/navigationpro/MainActivity.kt
+++ app/src/main/java/com/example/navigationpro/MainActivity.kt
@@ -7,23 +7,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.navigationpro.ui.theme.NavigationProTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             NavigationProTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
-                    innerPadding ->
-                    NavigationHost(innerPadding)
+                val navController = rememberNavController()
+                var title by remember { mutableStateOf("Home") }
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        CenterAlignedTopAppBar(title = { Text(title) })
+                    }
+                ) { innerPadding ->
+                    NavigationHost(navController, innerPadding) { newTitle ->
+                        title = newTitle
+                    }
                 }
             }
         }
@@ -31,15 +46,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun NavigationHost(innerPadding: PaddingValues) {
-    val navController = rememberNavController()
-
+fun NavigationHost(
+    navController: NavHostController,
+    innerPadding: PaddingValues,
+    setTitle: (String) -> Unit
+) {
     NavHost(
         navController = navController,
         startDestination = "FirstScreen",
         modifier = Modifier.padding(innerPadding)
     ) {
-        composable("FirstScreen") { FirstScreen(navController) }
-        composable("SecondScreen") { SecondScreen(navController) }
+        composable("FirstScreen") {
+            setTitle("First Screen")
+            FirstScreen(navController)
+        }
+        composable("SecondScreen") {
+            setTitle("Second Screen")
+            SecondScreen(navController)
+        }
     }
 }
--- app/src/main/java/com/example/navigationpro/SecondScreen.kt
+++ app/src/main/java/com/example/navigationpro/SecondScreen.kt
@@ -5,28 +5,32 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SecondScreen(navController: NavHostController){
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(15.dp)
-    ) {
-        Text(text = "Second Screen")
+fun SecondScreen(navController: NavHostController) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(5.dp)
+        ) {
+            Text(text = "Second Screen")
 
-        Button(onClick = { navController.navigate("FirstScreen")}) {
-            Text(text = " Go to the first screen")
+            Button(onClick = { navController.navigate("FirstScreen") }) {
+                Text(text = "Go to the First Screen")
+            }
         }
     }
-
-}
-
